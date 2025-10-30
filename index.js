83b1baace4bb1f5f43475e4669427cbffa61aa4b
Juliomozol <98849634+Juliomozol@users.noreply.github.com>
2025-10-30 10:38:34 -0300

    Remove Discord.js client setup from index.js
    Removed Discord.js client initialization and dotenv configuration.

--- index.js
+++ index.js
@@ -2,18 +2,6 @@
 // Linguagem: JavaScript (Node.js)
 // Discord.js v14+
 
-require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-  ],
-});
-
 client.once("ready", () => {
   console.log(`🤖 Logado como ${client.user.tag}`);
 });
